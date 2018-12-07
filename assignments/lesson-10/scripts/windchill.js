ea7245374dfe5d230d98780affd7c1ebe10a35e8
Artem Karpikov <41835817+artyom14@users.noreply.github.com>
2018-12-07 21:46:19 +0300

    Update windchill.js

--- assignments/lesson-10/scripts/windchill.js
+++ assignments/lesson-10/scripts/windchill.js
@@ -2,4 +2,4 @@ var temperature, speed, chill;
 speed = 5^0.16;
 temperature = 66;
 chill = 35.74 + 0.6215 * temperature - 35.75 * speed + 0.4275 * temperature * speed;
-document.getElementById("windchill").innerHTML = "Wind Chill: " + chill + " F";
+document.getElementById("windchill").innerHTML = chill;
