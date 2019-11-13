014942db6eec4cc87603f6aad6f3276f7a5c688e
Artem Karpikov <41835817+artyom14@users.noreply.github.com>
2019-11-14 00:14:47 +0300

    Add files via upload

---
+++ assignments/Lesson-8/js/windchill.js
@@ -0,0 +1,5 @@
+var temperature, speed, chill;
+speed = 5^0.16;
+temperature = 66;
+chill = 35.74 + 0.6215 * temperature - 35.75 * speed + 0.4275 * temperature * speed;
+document.getElementById("windchill").innerHTML = chill;
