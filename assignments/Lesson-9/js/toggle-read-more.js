af1d9a4997ed98362b315c964e269ee5f8502306
Artem Karpikov <41835817+artyom14@users.noreply.github.com>
2019-11-18 01:41:57 +0300

    Add files via upload

---
+++ assignments/Lesson-9/js/toggle-read-more.js
@@ -0,0 +1,4 @@
+function toggleFullArticle() {
+    document.getElementsByClassName("full-article")[0].classList.toggle("displayed");
+    document.getElementsByClassName("disp-full-artcile")[0].classList.toggle("hide");
+}
